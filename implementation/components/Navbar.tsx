000de04dd1206abda8ae43d85cb0b532c60a84d3
Aditya Saxena <iam_adisxn@outlook.com>
2025-06-24 06:19:54 -0400

    improved pdf

--- implementation/components/Navbar.tsx
+++ implementation/components/Navbar.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='text-blue-900 font-bold mt-4 text-xl bg-green-500'>Navbar</div>
+    <div className=''>
+        <div className='h-16 w-16 rounded-full bg-blue-700'></div>
+        <div className='h-36 w-16 rounded-full bg-red-700'></div>
+        <div className='h-16 w-16 rounded-full bg-green-700'></div>
+    </div>
+
   )
 }
 
